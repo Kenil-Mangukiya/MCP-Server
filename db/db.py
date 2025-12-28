704aceddb2c0d1a88690f53f3b910861c90fb060
Kenil-Mangukiya <kenilmangukiya033@gmail.com>
2025-12-28 11:28:31 +0530

    initialized db connection

---
+++ db/db.py
@@ -0,0 +1,22 @@
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGODB_URI = os.getenv("MONGODB_URI")
+print(f"Mongodb URI is : {MONGODB_URI}")
+
+if not MONGODB_URI:
+    raise RuntimeError("❌ MONGODB_URI is not set in .env file")
+
+client = MongoClient(MONGODB_URI)
+
+db = client.get_default_database()
+
+def get_db():
+    """
+    Returns the MongoDB database instance.
+    This connection is reused by all MCP tools.
+    """
+    return db
